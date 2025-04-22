3aa3f815309e52cf0f2a0fa24706cf6d8fc0c6a5
Sai Madhuri Kandula <saimadhurikandula@gmail.com>
2025-04-22 15:45:22 -0700

    project structure ready and venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Sai Madhuri Kandula",
+    author_email="saimadhurikandula@gmail.com",
+    packages=find_packages()
+)
